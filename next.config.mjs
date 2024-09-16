9299534bcf6350ee299a0fde243e8a0fd1d79ccb
RajuGangitla <gangitlaraju8520@gmail.com>
2024-09-16 17:52:36 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,16 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+        port: '',
+      },
+    ],
+  },
 };
 
 export default withMDX(config);
